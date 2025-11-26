bf4b439eeafe1f064ea1df5235fedd2e67521732
Victor Terra <victorterradev@gmail.com>
2025-11-26 14:53:37 -0300

    chore: adiciona segurança no header com helmet

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import postsRoutes from './routes/postRoutes.js';
 import express from 'express';
 import cors from 'cors';
+import helmet from 'helmet';
 
 const app = express(); // middleware para aceitar JSON nas requisições
 
 app.use(express.json());
 app.use(cors());
+app.use(helmet());
 
 app.use(postsRoutes); // usa as rotas de posts
 
