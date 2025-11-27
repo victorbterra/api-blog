e6bf0dca7d64c36df960f543ff8a24422e64304b
Victor Terra <victorterradev@gmail.com>
2025-11-27 11:31:29 -0300

    refactor: ajusta mensagem de resposta no arquivo de middleware

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -5,13 +5,13 @@ dotenv.config();
 
 const authMiddleware = (req, res, next) => {
     const authHeader = req.header("Authorization");
-    if (!authHeader) return res.status(401).json({ message: "Acesso negado" });
+    if (!authHeader) return res.status(401).json({ message: "Acesso negado. Token não fornecido" });
     try {
         const verified = jwt.verify(authHeader.split(" ")[1], process.env.JWT_SECRET);
         req.user = verified;
         next();
     } catch (error) {
-        res.status(400).json({ message: "Token inválido" });
+        res.status(400).json({ message: "Token inválido ou expirado. Verifique o token e tente novamente." });
     }
 };
 
