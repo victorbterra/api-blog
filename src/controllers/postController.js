761d78e54f0495bfc4a8713318d8ae595bd931ac
Victor Terra <victorterradev@gmail.com>
2025-12-01 14:42:15 -0300

    Fix: Ajusta lógica de verificar se há categoria existente

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -1,112 +1,145 @@
 import Post from "../models/postModel.js";
 import Category from "../models/categoryModel.js";
 
-
 class PostController {
-    static async createPost(req, res) {
-        try {
-            let {title, content, author, slug, tags, category} = req.body;
-            if(!category) {
-                let defaultCategory = await Category.findOne({ slug: 'Geral' });
-                if(!defaultCategory) {
-                    defaultCategory = new Category({ name: 'Geral', slug: 'geral' });
-                    await defaultCategory.save();
-                }
-                category = defaultCategory._id;
-            }
-            const newPost = new Post({
-                title,
-                content,
-                author,
-                slug,
-                tags,
-                category
+  static async createPost(req, res) {
+    try {
+        let { title, content, author, slug, tags, category } = req.body;
+        if (!category) {
+            let defaultCategory = await Category.findOne({
+            $or: [{ slug: "geral" }, { name: "Geral" }],
             });
-            await newPost.save();
-            res.status(201).json({message: 'Post criado com sucesso!', Post: newPost});
-        } catch (error) {
-            res.status(500).json({ message:'Erro ao criar o post. ', error: error.message });
+
+        if (!defaultCategory) {
+            defaultCategory = await Category.create({
+            name: "Geral",
+            slug: "geral",
+            });
         }
+        category = defaultCategory._id;
+        }
+        const newPost = new Post({
+        title,
+        content,
+        author,
+        slug,
+        tags,
+        category,
+        });
+        await newPost.save();
+        res
+            .status(201)
+            .json({ message: "Post criado com sucesso!", Post: newPost });
+    } catch (error) {
+        res
+            .status(500)
+            .json({ message: "Erro ao criar o post. ", error: error.message });
+    }
     }
 
-    static async getPosts(req, res) {
-        
-        try {
-            const {title} = req.query;
-            let filter = {};
-            if (title) {
-                filter.title = { $regex: title, $options: 'i' }; // busca por título com regex para permitir buscas parciais e case insensitive
-            }
-            let {page = 1, limit = 10} = req.query; // paginação
-            page = parseInt(page);
-            limit = parseInt(limit);
-            const posts = await Post.find(filter)
-                .populate("category")
-                .skip((page - 1) * limit) // Calcula o número de documentos a serem pulados com base na página atual e no limite
-                .limit(limit);
- // popula os dados da categoria
-                
-            res.status(200).json({message: 'Posts retornados com sucesso!', posts: posts});
-        } catch (error) {
-            res.status(500).json({ message: 'Erro ao buscar os posts.', error: error.message });
-        }
-    }
+  static async getPosts(req, res) {
+    try {
+      const { title } = req.query;
+      let filter = {};
+      if (title) {
+        filter.title = { $regex: title, $options: "i" }; // busca por título com regex para permitir buscas parciais e case insensitive
+      }
+      let { page = 1, limit = 10 } = req.query; // paginação
+      page = parseInt(page);
+      limit = parseInt(limit);
+      const posts = await Post.find(filter)
+        .populate("category")
+        .skip((page - 1) * limit) // Calcula o número de documentos a serem pulados com base na página atual e no limite
+        .limit(limit);
+      // popula os dados da categoria
 
-    static async getPostById(req, res) {
-        try{
-            const postId = req.params.id;
-            const findPostId = await Post.findById(postId);
-            if(findPostId){
-                res.status(200).json({message: 'Post encontrado com sucesso!', post: findPostId});
-            } else {
-                res.status(404).json({message: 'Post não encontrado.'});
-            }
-        } catch (error) {
-            res.status(500).json({ message: 'Erro ao buscar o post.', error: error.message });
-        }
+      res
+        .status(200)
+        .json({ message: "Posts retornados com sucesso!", posts: posts });
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: "Erro ao buscar os posts.", error: error.message });
     }
-    static async getPostBySlug(req, res) {
-        try{
-            const postSlug = req.params.slug;
-            const findPostSlug = await Post.findOne({slug: postSlug})
-                .populate("category");
-            if(findPostSlug) {
-                res.status(200).json({message: 'Post encontrado com sucesso!', post: findPostSlug});
-            } else {
-                res.status(404).json({message: 'Post não encontrado.'});
-            }
-        } catch (error) {
-            res.status(500).json({ message: 'Erro ao buscar o post.', error: error.message });
-        }
-    }
+  }
 
-    static async updatePost(req, res) {
-        try{
-            const postId = req.params.id;
-            const updatedPost = await Post.findByIdAndUpdate(postId, req.body, { new: true }); //
-            if(updatedPost) {
-                res.status(200).json({message: 'Post atualizado com sucesso!', post: updatedPost});
-            }else {
-                res.status(404).json({message: 'Post não encontrado.'});
-            }
-        } catch (error) {
-            res.status(500).json({ message: 'Erro ao atualizar o post.', error: error.message });
-        }
-    } 
-
-    static async deletePost(req, res) {
-        try{
-            const postId = req.params.id;
-            const deletedPost = await Post.findByIdAndDelete(postId);
-            if(deletedPost) {
-                res.status(200).json({message: 'Post deletado com sucesso!', post: deletedPost});
-            } else {
-                res.status(404).json({message: 'Post não encontrado.'});
-            }
-        } catch (error) {
-            res.status(500).json({ message: 'Erro ao deletar o post.', error: error.message });
-        }
+  static async getPostById(req, res) {
+    try {
+      const postId = req.params.id;
+      const findPostId = await Post.findById(postId);
+      if (findPostId) {
+        res
+          .status(200)
+          .json({ message: "Post encontrado com sucesso!", post: findPostId });
+      } else {
+        res.status(404).json({ message: "Post não encontrado." });
+      }
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: "Erro ao buscar o post.", error: error.message });
     }
+  }
+  static async getPostBySlug(req, res) {
+    try {
+      const postSlug = req.params.slug;
+      const findPostSlug = await Post.findOne({ slug: postSlug }).populate(
+        "category"
+      );
+      if (findPostSlug) {
+        res
+          .status(200)
+          .json({
+            message: "Post encontrado com sucesso!",
+            post: findPostSlug,
+          });
+      } else {
+        res.status(404).json({ message: "Post não encontrado." });
+      }
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: "Erro ao buscar o post.", error: error.message });
+    }
+  }
+
+  static async updatePost(req, res) {
+    try {
+      const postId = req.params.id;
+      const updatedPost = await Post.findByIdAndUpdate(postId, req.body, {
+        new: true,
+      }); //
+      if (updatedPost) {
+        res
+          .status(200)
+          .json({ message: "Post atualizado com sucesso!", post: updatedPost });
+      } else {
+        res.status(404).json({ message: "Post não encontrado." });
+      }
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: "Erro ao atualizar o post.", error: error.message });
+    }
+  }
+
+  static async deletePost(req, res) {
+    try {
+      const postId = req.params.id;
+      const deletedPost = await Post.findByIdAndDelete(postId);
+      if (deletedPost) {
+        res
+          .status(200)
+          .json({ message: "Post deletado com sucesso!", post: deletedPost });
+      } else {
+        res.status(404).json({ message: "Post não encontrado." });
+      }
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: "Erro ao deletar o post.", error: error.message });
+    }
+  }
 }
 
 export default PostController;
