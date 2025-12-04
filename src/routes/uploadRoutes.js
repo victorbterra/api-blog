a7e264c43e8f2949d24a6028ca3e8d927911df43
Victor Terra <victorterradev@gmail.com>
2025-12-04 12:28:14 -0300

    Refactor: configuração da rota de upload de imagens

--- src/routes/uploadRoutes.js
+++ src/routes/uploadRoutes.js
@@ -1,11 +1,29 @@
 import express from 'express';
-import cloudinaryConfig from '../config/cloudinary.js';
 import multer from 'multer';
+import cloudinary from 'cloudinary';
+import dotenv from 'dotenv';
+import { CloudinaryStorage } from 'multer-storage-cloudinary';
 
-
+dotenv.config();
 const router = express.Router();
 
-const upload = multer({ storage: cloudinaryConfig });
+// Configuração do Cloudinary
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+// Configuração do Armazenamento (Multer + Cloudinary)
+const storage = new CloudinaryStorage({
+    cloudinary: cloudinary,
+    params: {
+      folder: 'blog-terratech', // Nome da pasta no Cloudinary
+      allowed_formats: ['jpg', 'png', 'jpeg', 'webp'],
+    },
+});
+
+const upload = multer({ storage: storage });
 
 
 router.post('/upload', upload.single('image'), (req, res) => {
